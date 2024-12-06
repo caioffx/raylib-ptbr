fd5e72f8d5936cb4b376494e649c79dc13514d02
caio f f <caiofkreitas@gmail.com>
2024-12-05 22:29:26 -0300

    Tradução do exemplo "text_format_text.c".
    Tradução direta de exemplo da biblioteca Raylib, além de comentários adicionais sobre o funcionamento das funções da Raylib para fins de didática e maior acesso.

--- examples/text/text_format_text.c
+++ examples/text/text_format_text.c
@@ -1,66 +1,71 @@
 /*******************************************************************************************
 *
-*   raylib [text] example - Text formatting
+*   raylib [text] exemplo - Formatação de texto
 *
-*   Example originally created with raylib 1.1, last time updated with raylib 3.0
+*   Exemplo originalmente criado com raylib 1.1, última atualização com raylib 3.0
 *
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
+*   Exemplo licenciado sob a licença zlib/libpng não modificada, que é uma licença certificada pela OSI, 
+*   similar à BSD, que permite a vinculação estática com software de código fechado
 *
 *   Copyright (c) 2014-2024 Ramon Santamaria (@raysan5)
+*   Tradução e comentários adicionais por Caio Fabio (@caioffx)
 *
 ********************************************************************************************/
 
 #include "raylib.h"
 
 //------------------------------------------------------------------------------------
-// Program main entry point
+// Ponto de entrada principal do programa
 //------------------------------------------------------------------------------------
 int main(void)
 {
-    // Initialization
+    // Inicialização
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 800;    // Largura da tela
+    const int screenHeight = 450;   // Altura da tela
 
-    InitWindow(screenWidth, screenHeight, "raylib [text] example - text formatting");
+    // Inicializa a janela com o título
+    InitWindow(screenWidth, screenHeight, "raylib [text] exemplo - Formatação de texto");
 
-    int score = 100020;
+    // Cria variáveis que guardam valores atribuídos a elas que posteriormente serão exibidos na tela
+    int score = 100020; 
     int hiscore = 200450;
     int lives = 5;
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    SetTargetFPS(60);               // Define o FPS do jogo para 60 (jogo vai rodar a 60 quadros por segundo)
     //--------------------------------------------------------------------------------------
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    // Loop do algoritmo principal
+    while (!WindowShouldClose())    // Detecta o fechamento da janela ou pressionamento da tecla ESC
     {
-        // Update
+        // Atualizar/modificar
         //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
+        // Para fazer: atualize e insira suas variáveis aqui
         //----------------------------------------------------------------------------------
 
-        // Draw
+        // Desenhar o conteúdo da janela
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
 
-            DrawText(TextFormat("Score: %08i", score), 200, 80, 20, RED);
+            // DrawText irá desenhar o texto resultante na tela com posição, tamanho e cor definidos. Junto da TextFormat, que gera o texto dinamicamente a partir dos valores das variáveis score, hiscore, lives, e GetFrameTime(). 
 
-            DrawText(TextFormat("HiScore: %08i", hiscore), 200, 120, 20, GREEN);
+            DrawText(TextFormat("Pontuação: %08i", score), 200, 80, 20, RED);
 
-            DrawText(TextFormat("Lives: %02i", lives), 200, 160, 40, BLUE);
+            DrawText(TextFormat("Maior pontuação: %08i", hiscore), 200, 120, 20, GREEN);
 
-            DrawText(TextFormat("Elapsed Time: %02.02f ms", GetFrameTime()*1000), 200, 220, 20, BLACK);
+            DrawText(TextFormat("Vidas: %02i", lives), 200, 160, 40, BLUE);
+
+            DrawText(TextFormat("Tempo decorrido: %02.02f ms", GetFrameTime()*1000), 200, 220, 20, BLACK);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
+    // Finalização
     //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
+    CloseWindow();        // Fecha a janela e o contexto OpenGL
     //--------------------------------------------------------------------------------------
 
     return 0;
